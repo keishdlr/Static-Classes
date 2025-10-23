502fae7a04d70af7c78879abc5e38bf171dc17fa
keisha De La Rosa <keishdlr@gmail.com>
2025-10-23 11:24:35 -0400

    added static methods

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -2,8 +2,40 @@ package com.pluralsight;
 
 public class NameFormatter {
 
-    //private constructor so I don't have to instantiate a class
-    private NameFormatter(){
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String prefix;
+    private String suffix;
+    private String fullName;
 
+    //private constructor so I cannot instantiate a class
+    private NameFormatter(){
+        System.out.println("Your Name is " +
+                prefix + "" +
+                firstName + "" +
+                middleName + "" +
+                lastName + "" +
+                suffix);
+    }
+
+    public static String format (String firstName, String lastName){
+
+
+        return firstName + " " + lastName;
+    }
+
+    public static  String format(String prefix, String firstName, String middleName, String lastName, String suffix){
+
+
+        return prefix + " " + firstName + " " + middleName + " " + lastName + " " + suffix;
+    }
+
+    public static String format (String fullName){
+        //use split to separate the name by space
+        String[] parts = fullName.split(" ");
+
+
+        return // some value;
     }
 }
