b6d5c76ec6c0fa0a9bd779f6fc6ef1285188abf9
keisha De La Rosa <keishdlr@gmail.com>
2025-10-23 10:39:59 -0400

    first commit

---
+++ src/main/java/com/pluralsight/LibraryofNames.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class LibraryofNames {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class NameFormatter {
+
+    //private constructor so I don't have to instantiate a class
+    private NameFormatter(){
+
+    }
+}
